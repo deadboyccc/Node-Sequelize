0a11620f9e488a951c48140d6018648909f39da7
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-01-22 10:38:15 +0300

    organized gitignore

--- src/routers/tagRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { createTag, getTags } from "../controllers/tagControllers"
-
-const tagRouter = Router()
-
-tagRouter.get("/", getTags)
-tagRouter.post("/", createTag)
-
-export default tagRouter
--- src/routers/viewRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { indexViewController } from "../controllers/viewController"
-
-const viewRouter = express.Router()
-
-// Get all users and render 'index' template
-viewRouter.get("/", indexViewController)
-export default viewRouter
